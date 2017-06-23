b64b6e5b166cc20c30200621a6f6946f4cbb55cf
nyangry <iacta.alea.esto@gmail.com>
2017-06-23 10:10:03 +0900

    Implement index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   let canvas = new fabric.Canvas('canvas')
 
-  let $canvas_width_input  = document.getElementById('canvas-width-input')
-  let $canvas_height_input = document.getElementById('canvas-height-input')
+  // define app elements
+  let elements = {}
+  elements.$canvas_width_input  = document.getElementById('canvas-width-input')
+  elements.$canvas_height_input = document.getElementById('canvas-height-input')
 
   // Initialize
   {
     // set canvas size into witdh and height setting
-    $canvas_width_input.value  = canvas.getWidth()
-    $canvas_height_input.value = canvas.getHeight()
+    elements.$canvas_width_input.value  = canvas.getWidth()
+    elements.$canvas_height_input.value = canvas.getHeight()
   }
 })
