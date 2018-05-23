302adb0d7399c261d8f7e4713c0772151f36f462
nyangry <iacta.alea.esto@gmail.com>
2018-05-23 11:35:40 +0900

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  }
+}
